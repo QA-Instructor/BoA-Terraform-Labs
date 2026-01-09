a5d0a46d9caa74750ae7bd35f4f81ef28d136f65
Victoria Lloyd <qainstructorsky@gmail.com>
2026-01-09 12:15:03 +0000

    CREATE: TF05

---
+++ TF05/variables.tf
@@ -0,0 +1,18 @@
+variable "lab-project" {
+  default = "qwiklabs-gcp-00-39895d52dfdc"
+}
+variable "lab-region" {
+  default = "us-east1"
+}
+
+variable "instance-prefix" {
+  default = "lab-template-"
+}
+
+variable "machine-type" {
+  default = "e2-small"
+}
+
+variable "lab-zone" {
+  default = "us-east1-b"
+}
